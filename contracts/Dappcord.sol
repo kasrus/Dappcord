2c05c8e7b396e28e3c633923b2921a59ee33bb2d
kasrus <kruslim@gmail.com>
2023-03-10 19:30:11 -0800

    first commit

--- contracts/Dappcord.sol
+++ contracts/Dappcord.sol
@@ -1,4 +1,56 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
 
-contract Dappcord {}
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+contract Dappcord is ERC721 {
+    uint public totalChannels;
+    uint public totalSupply;
+    address public owner;
+
+    struct Channel {
+        uint id;
+        string name;
+        uint cost;
+    }
+
+    mapping (uint=> Channel) public channels;
+    mapping (uint => mapping(address => bool)) public hasJoined;
+
+    modifier onlyOwner() {
+        require(owner == msg.sender);
+        _;
+    }
+
+    constructor(string memory _name, string memory _symbol) ERC721(_name, _symbol) {
+        owner = msg.sender;
+    }
+
+    function createChannel(string memory _name, uint _cost) public onlyOwner {
+        totalChannels++;
+        channels[totalChannels] = Channel(totalChannels, _name, _cost);
+    }
+
+    function mint(uint _id) public payable {
+        require(_id != 0);
+        require(_id <= totalChannels);
+        require(hasJoined[_id][msg.sender] == false);
+        require(msg.value >= channels[_id].cost);
+
+        //Join channel
+        hasJoined[_id][msg.sender] = true;
+
+        //Mint NFT
+        totalSupply++;
+        _safeMint(msg.sender, totalSupply);
+    }
+    
+    function getChannel(uint _id) public view returns(Channel memory) {
+        return channels[_id];
+    }
+
+    function withdraw() public onlyOwner {
+        (bool success, ) = owner.call{value: address(this).balance}("");
+        require(success);
+    }
+}
--- test/Dappcord.js
+++ test/Dappcord.js
@@ -4,6 +4,119 @@ const tokens = (n) => {
   return ethers.utils.parseUnits(n.toString(), 'ether')
 }
 
-describe("Dappcord", function () {
+const NAME = "Dappcord";
+const SYMBOL = "DC";
 
+describe("Dappcord", ()=> {
+  let dappcord;
+  let deployer, user;
+
+  beforeEach(async() => {
+    //Get the accounts connected to the blockchain
+    [deployer, user] = await ethers.getSigners();
+    const Dappcord = await ethers.getContractFactory(NAME);
+    dappcord = await Dappcord.deploy(NAME, SYMBOL);
+    
+    //Create a channel
+    const transaction = await dappcord.connect(deployer).createChannel("general", tokens(1));
+    await transaction.wait();
+  })
+
+  describe("Deployment", () => {
+    it("Sets the name", async() => {
+      expect(await dappcord.name()).to.equal(NAME);
+    })
+
+    it("Sets the symbol", async() => {
+      expect(await dappcord.symbol()).to.equal(SYMBOL);
+    })
+
+    it("Sets the owner", async() => {
+      expect(await dappcord.owner()).to.equal(deployer.address);
+    })
+  })
+  
+  describe("Creating Channels", () => {
+    it("Returns the total number of the channels", async() => {
+      expect(await(dappcord).totalChannels()).to.equal(1);
+    })
+
+    it("Returns channel attributes", async() => {
+      const channel = await dappcord.getChannel(1);
+      expect(await channel.id).to.equal(1);
+      expect(await channel.name).to.equal("general");
+      expect(await channel.cost).to.equal(tokens(1));
+    })
+  })
+
+  describe("Joining Channels", () => {
+    const ID = 1;
+    const AMOUNT = tokens(1);
+    let transaction;
+
+    beforeEach(async() => {    
+      transaction = await dappcord.connect(user).mint(ID, { value: AMOUNT });
+      await transaction.wait();
+    })
+
+    describe("Success", async() => { 
+      it("Joins the user", async() => {
+        expect(await dappcord.hasJoined(ID, user.address)).to.equal(true);
+      })
+  
+      it("Increases total supply", async() => {
+        expect(await dappcord.totalSupply()).to.equal(1);
+      })
+  
+      it ("Updates the contract balance", async() => {
+        const result = await ethers.provider.getBalance(dappcord.address);
+        expect(result).to.equal(AMOUNT);
+      })
+    })
+    
+    describe("Failure", async() => {
+      it("Rejects when channel id is 0", async() => {
+        await expect(dappcord.connect(user).mint(0, { value: AMOUNT })).to.be.reverted;
+      })
+
+      it("Rejects when channel id is greater than the number of channels", async() => {
+        await expect(dappcord.connect(user).mint(10, { value: AMOUNT })).to.be.reverted;
+      })
+
+      it("Rejects when the user already joined the channel", async() => {
+        await expect(dappcord.connect(user).mint(ID, { value: AMOUNT})).to.be.reverted;
+      })
+
+      it("Rejects when insuficient fund", async() => {
+        const transaction = await dappcord.connect(deployer).createChannel("testing", tokens(10));
+        await transaction.wait();
+        await expect(dappcord.connect(user).mint(2, { value: AMOUNT})).to.be.reverted;
+      })
+    })
+  })
+
+  describe("Withdrawing ethers", () => {
+    const ID = 1;
+    const AMOUNT = tokens(10);
+    let balanceBefore;
+
+    beforeEach(async() => {
+      balanceBefore = await ethers.provider.getBalance(deployer.address);
+      
+      let transaction = await dappcord.connect(user).mint(ID, { value: AMOUNT });
+      await transaction.wait();
+
+      transaction = await dappcord.connect(deployer).withdraw();
+      await transaction.wait();
+    })
+
+    it("Updates the owner balance", async() => {
+      let balanceAfter = await ethers.provider.getBalance(deployer.address);
+      expect(balanceAfter).to.be.greaterThan(balanceBefore);
+    })
+
+    it("Updates the contract balance", async() => {
+      expect(await ethers.provider.getBalance(dappcord.address)).to.equal(0);
+    })
+  })
 })
